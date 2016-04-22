eb430e7f9d3fd9e31cc6b3a71473cad8688dea27
Hieu Tran <tr.kimhieu@gmail.com>
2016-04-22 16:59:46 +0700

    Fix code style issues

--- app/src/main/java/px500/pipoask/com/adapter/holder/MainHolder.java
+++ app/src/main/java/px500/pipoask/com/adapter/holder/MainHolder.java
@@ -3,9 +3,6 @@ package px500.pipoask.com.adapter.holder;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 
-/**
- * Created by Sandy on 12/27/15.
- */
 public class MainHolder extends RecyclerView.ViewHolder implements  View.OnClickListener{
 
     protected ClickListener listener;
@@ -27,6 +24,6 @@ public class MainHolder extends RecyclerView.ViewHolder implements  View.OnClick
     }
 
     public interface ClickListener {
-        public void onItemClicked(View v, int position);
+        void onItemClicked(View v, int position);
     }
 }
--- app/src/main/java/px500/pipoask/com/module/photo/IPhotoView.java
+++ app/src/main/java/px500/pipoask/com/module/photo/IPhotoView.java
@@ -3,11 +3,10 @@ package px500.pipoask.com.module.photo;
 import px500.pipoask.com.data.model.Photo;
 import px500.pipoask.com.module.base.MvpView;
 
-/**
- * Created by Sandy on 12/26/15.
- */
 public interface IPhotoView extends MvpView {
-    public void loadingImage();
-    public void imageLoaded(Photo photo);
-    public void errorLoadingImage(String e);
+    void loadingImage();
+
+    void imageLoaded(Photo photo);
+
+    void errorLoadingImage(String e);
 }
